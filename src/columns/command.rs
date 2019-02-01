c12e51445fa90e5b7a4e42e939f9cf337d666eee
dalance <dalance@gmail.com>
2019-02-01 19:37:31 +0900

    Fix command with \n

--- src/columns/command.rs
+++ src/columns/command.rs
@@ -46,7 +46,7 @@ impl Column for Command {
                     })
                     .collect::<String>();
                 cmd.pop();
-                cmd = cmd.replace("\n", "  ");
+                cmd = cmd.replace("\n", "");
                 cmd
             } else {
                 curr_proc.stat.comm.clone()
