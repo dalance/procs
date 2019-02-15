b02fa96e2a2d66ed9cba99e05c82c5f80479824f
dalance <dalance@gmail.com>
2019-02-16 01:33:38 +0900

    Fix

--- src/config.rs
+++ src/config.rs
@@ -393,7 +393,7 @@ pub struct ConfigPager {
 impl Default for ConfigPager {
     fn default() -> Self {
         ConfigPager {
-            mode: ConfigPagerMode::Auto,
+            mode: ConfigPagerMode::Disable,
             command: None,
         }
     }
