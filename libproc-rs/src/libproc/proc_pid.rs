bf0009c9d3998d9da730a42d0932831178efb88e
dalance <dalance@gmail.com>
2019-02-21 14:50:10 +0900

    Fix

--- libproc-rs/src/libproc/proc_pid.rs
+++ libproc-rs/src/libproc/proc_pid.rs
@@ -206,6 +206,14 @@ pub enum PidFDInfoFlavor {
     ATalkInfo       = 8
 }
 
+// From https://opensource.apple.com/source/xnu/xnu-2782.30.5/bsd/sys/resource.h
+pub enum PidRUsageFlavor {
+    V0 = 0,
+    V1 = 1,
+    V2 = 2,
+    V3 = 3,
+}
+
 // this extern block links to the libproc library
 // Original signatures of functions can be found at http://opensource.apple.com/source/Libc/Libc-594.9.4/darwin/libproc.c
 #[link(name = "proc", kind = "dylib")]
@@ -223,6 +231,8 @@ extern {
     fn proc_pidpath(pid: c_int, buffer: *mut c_void, buffersize: uint32_t) -> c_int;
 
     fn proc_libversion(major: *mut c_int, minor: *mut c_int) -> c_int;
+
+    fn proc_pid_rusage(pid: c_int, flavor: c_int, buffer: *mut c_void) -> c_int;
 }
 
 
@@ -643,7 +653,7 @@ pub trait PIDFDInfo: Default {
     fn flavor() -> PidFDInfoFlavor;
 }
 
-/// Returns the information of the process that match pid passed in.
+/// Returns the information about file descriptors of the process that match pid passed in.
 ///
 /// # Examples
 ///
@@ -686,11 +696,6 @@ pub trait PIDFDInfo: Default {
 ///                                     addr |= s_addr << 24 & 0xff000000;
 ///
 ///                                     println!("{}.{}.{}.{}:{}", addr >> 24 & 0xff, addr >> 16 & 0xff, addr >> 8 & 0xff, addr & 0xff, port);
-///                                     assert_eq!(port, 8000);
-///                                     assert_eq!(addr>>24&0xff, 127);
-///                                     assert_eq!(addr>>16&0xff, 0  );
-///                                     assert_eq!(addr>>8 &0xff, 0  );
-///                                     assert_eq!(addr>>0 &0xff, 1  );
 ///                                 },
 ///                                 _ => (),
 ///                             }
@@ -1089,5 +1094,159 @@ impl Default for KernCtlInfo {
             kcsi_name       : [0; MAX_KCTL_NAME],
         }
     }
+}
+
+// This trait is needed for polymorphism on pidrusage types, also abstracting flavor in order to provide
+// type-guaranteed flavor correctness
+pub trait PIDRUsage: Default {
+    fn flavor() -> PidRUsageFlavor;
+}
+
+/// Returns the information about resources of the process that match pid passed in.
+///
+/// # Examples
+///
+/// ```
+/// use std::io::Write;
+/// use std::net::TcpListener;
+/// use libproc::libproc::proc_pid::{listpidinfo, pidinfo, pidfdinfo, ListFDs, ListThreads, BSDInfo, ProcFDType, SocketFDInfo, SocketInfoKind};
+///
+/// fn pidrusage_test() {
+///     use std::process;
+///     let pid = process::id() as i32;
+///
+///     if let Ok(res) = pidrusage::<RUsageInfoV3>(pid) {
+///         println!("UUID: {:?}, Disk Read: {}, Disk Write: {}", res.ri_uuid, res.ri_diskio_bytesread, res.ri_diskio_byteswritten);
+///     }
+/// }
+/// ```
+///
+pub fn pidrusage<T: PIDRUsage>(pid : i32) -> Result<T, String> {
+    let flavor = T::flavor() as i32;
+    let buffer_size = mem::size_of::<T>() as i32;
+    let mut pidrusage = T::default();
+    let buffer_ptr = &mut pidrusage as *mut _ as *mut c_void;
+    let ret: i32;
+
+    unsafe {
+        ret = proc_pid_rusage(pid, flavor, buffer_ptr);
+    };
+
+    if ret <= 0 {
+        Err(get_errno_with_message(ret))
+    } else {
+        Ok(pidrusage)
+    }
+}
+
+#[repr(C)]
+#[derive(Default)]
+pub struct RUsageInfoV0 {
+    pub ri_uuid              : [uint8_t; 16],
+    pub ri_user_time         : uint64_t,
+    pub ri_system_time       : uint64_t,
+    pub ri_pkg_idle_wkups    : uint64_t,
+    pub ri_interrupt_wkups   : uint64_t,
+    pub ri_pageins           : uint64_t,
+    pub ri_wired_size        : uint64_t,
+    pub ri_resident_size     : uint64_t,
+    pub ri_phys_footprint    : uint64_t,
+    pub ri_proc_start_abstime: uint64_t,
+    pub ri_proc_exit_abstime : uint64_t,
+}
+
+impl PIDRUsage for RUsageInfoV0 {
+    fn flavor() -> PidRUsageFlavor { PidRUsageFlavor::V0 }
+}
+
+#[repr(C)]
+#[derive(Default)]
+pub struct RUsageInfoV1 {
+    pub ri_uuid                 : [uint8_t; 16],
+    pub ri_user_time            : uint64_t,
+    pub ri_system_time          : uint64_t,
+    pub ri_pkg_idle_wkups       : uint64_t,
+    pub ri_interrupt_wkups      : uint64_t,
+    pub ri_pageins              : uint64_t,
+    pub ri_wired_size           : uint64_t,
+    pub ri_resident_size        : uint64_t,
+    pub ri_phys_footprint       : uint64_t,
+    pub ri_proc_start_abstime   : uint64_t,
+    pub ri_proc_exit_abstime    : uint64_t,
+    pub ri_child_user_time      : uint64_t,
+    pub ri_child_system_time    : uint64_t,
+    pub ri_child_pkg_idle_wkups : uint64_t,
+    pub ri_child_interrupt_wkups: uint64_t,
+    pub ri_child_pageins        : uint64_t,
+    pub ri_child_elapsed_abstime: uint64_t,
+}
+
+impl PIDRUsage for RUsageInfoV1 {
+    fn flavor() -> PidRUsageFlavor { PidRUsageFlavor::V1 }
+}
+
+#[repr(C)]
+#[derive(Default)]
+pub struct RUsageInfoV2 {
+    pub ri_uuid                 : [uint8_t; 16],
+    pub ri_user_time            : uint64_t,
+    pub ri_system_time          : uint64_t,
+    pub ri_pkg_idle_wkups       : uint64_t,
+    pub ri_interrupt_wkups      : uint64_t,
+    pub ri_pageins              : uint64_t,
+    pub ri_wired_size           : uint64_t,
+    pub ri_resident_size        : uint64_t,
+    pub ri_phys_footprint       : uint64_t,
+    pub ri_proc_start_abstime   : uint64_t,
+    pub ri_proc_exit_abstime    : uint64_t,
+    pub ri_child_user_time      : uint64_t,
+    pub ri_child_system_time    : uint64_t,
+    pub ri_child_pkg_idle_wkups : uint64_t,
+    pub ri_child_interrupt_wkups: uint64_t,
+    pub ri_child_pageins        : uint64_t,
+    pub ri_child_elapsed_abstime: uint64_t,
+    pub ri_diskio_bytesread     : uint64_t,
+    pub ri_diskio_byteswritten  : uint64_t,
+}
+
+impl PIDRUsage for RUsageInfoV2 {
+    fn flavor() -> PidRUsageFlavor { PidRUsageFlavor::V2 }
+}
+
+#[repr(C)]
+#[derive(Default)]
+pub struct RUsageInfoV3 {
+    pub ri_uuid                         : [uint8_t; 16],
+    pub ri_user_time                    : uint64_t,
+    pub ri_system_time                  : uint64_t,
+    pub ri_pkg_idle_wkups               : uint64_t,
+    pub ri_interrupt_wkups              : uint64_t,
+    pub ri_pageins                      : uint64_t,
+    pub ri_wired_size                   : uint64_t,
+    pub ri_resident_size                : uint64_t,
+    pub ri_phys_footprint               : uint64_t,
+    pub ri_proc_start_abstime           : uint64_t,
+    pub ri_proc_exit_abstime            : uint64_t,
+    pub ri_child_user_time              : uint64_t,
+    pub ri_child_system_time            : uint64_t,
+    pub ri_child_pkg_idle_wkups         : uint64_t,
+    pub ri_child_interrupt_wkups        : uint64_t,
+    pub ri_child_pageins                : uint64_t,
+    pub ri_child_elapsed_abstime        : uint64_t,
+    pub ri_diskio_bytesread             : uint64_t,
+    pub ri_diskio_byteswritten          : uint64_t,
+    pub ri_cpu_time_qos_default         : uint64_t,
+    pub ri_cpu_time_qos_maintenance     : uint64_t,
+    pub ri_cpu_time_qos_background      : uint64_t,
+    pub ri_cpu_time_qos_utility         : uint64_t,
+    pub ri_cpu_time_qos_legacy          : uint64_t,
+    pub ri_cpu_time_qos_user_initiated  : uint64_t,
+    pub ri_cpu_time_qos_user_interactive: uint64_t,
+    pub ri_billed_system_time           : uint64_t,
+    pub ri_serviced_system_time         : uint64_t,
+}
+
+impl PIDRUsage for RUsageInfoV3 {
+    fn flavor() -> PidRUsageFlavor { PidRUsageFlavor::V3 }
 }
 
