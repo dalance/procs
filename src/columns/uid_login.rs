d890187826882213e4d6d1076ccc6115c3808e00
dalance <dalance@gmail.com>
2019-10-21 10:41:00 +0900

    Add UidLogin/UserLogin #27

---
+++ src/columns/uid_login.rs
@@ -0,0 +1,41 @@
+use crate::process::ProcessInfo;
+use crate::{column_default, Column};
+use std::cmp;
+use std::collections::HashMap;
+
+pub struct UidLogin {
+    header: String,
+    unit: String,
+    fmt_contents: HashMap<i32, String>,
+    raw_contents: HashMap<i32, i32>,
+    width: usize,
+}
+
+impl UidLogin {
+    pub fn new() -> Self {
+        let header = String::from("LoginUID");
+        let unit = String::from("");
+        UidLogin {
+            fmt_contents: HashMap::new(),
+            raw_contents: HashMap::new(),
+            width: 0,
+            header,
+            unit,
+        }
+    }
+}
+
+impl Column for UidLogin {
+    fn add(&mut self, proc: &ProcessInfo) {
+        let (fmt_content, raw_content) = if let Ok(uid) = proc.curr_proc.loginuid() {
+            (format!("{}", uid), uid)
+        } else {
+            (String::from(""), 0)
+        };
+
+        self.fmt_contents.insert(proc.pid, fmt_content);
+        self.raw_contents.insert(proc.pid, raw_content);
+    }
+
+    column_default!(i32);
+}
