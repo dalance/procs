1408f4129aec893d2bdd11d15e21f89544f0f6cf
dalance <dalance@gmail.com>
2021-05-18 17:25:27 +0900

    Fix compile error

--- src/columns/elapsed_time.rs
+++ src/columns/elapsed_time.rs
@@ -2,9 +2,7 @@ use crate::process::ProcessInfo;
 use crate::{column_default, Column};
 #[cfg(not(target_os = "windows"))]
 use chrono::offset::TimeZone;
-#[cfg(target_os = "linux")]
-use chrono::Duration;
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Duration, Local};
 #[cfg(target_os = "linux")]
 use lazy_static::lazy_static;
 use std::cmp;
