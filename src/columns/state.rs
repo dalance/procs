8b8770ea3eb3ed5f1edb6fda88b6007aac600f8e
dalance <dalance@gmail.com>
2023-10-19 13:52:18 +0900

    Update bsd-kvm

--- src/columns/state.rs
+++ src/columns/state.rs
@@ -90,10 +90,10 @@ impl Column for State {
         let cr_flags = info.cr_flags;
         let kiflag = info.kiflag;
 
-        let mut state = match info.stat as u32 {
-            bsd_kvm_sys::SSTOP => "T",
-            bsd_kvm_sys::SSLEEP => {
-                if (tdflags & bsd_kvm_sys::TDF_SINTR as i64) != 0 {
+        let mut state = match info.stat {
+            libc::SSTOP => "T",
+            libc::SSLEEP => {
+                if (tdflags & libc::TDF_SINTR as i64) != 0 {
                     if info.slptime >= 20 {
                         "I"
                     } else {
@@ -103,45 +103,44 @@ impl Column for State {
                     "D"
                 }
             }
-            bsd_kvm_sys::SRUN | bsd_kvm_sys::SIDL => "R",
-            bsd_kvm_sys::SWAIT => "W",
-            bsd_kvm_sys::SLOCK => "L",
-            bsd_kvm_sys::SZOMB => "Z",
+            libc::SRUN | libc::SIDL => "R",
+            libc::SWAIT => "W",
+            libc::SLOCK => "L",
+            libc::SZOMB => "Z",
             _ => "?",
         }
         .to_string();
-        if (flag & bsd_kvm_sys::P_INMEM as i64) == 0 {
+        if (flag & libc::P_INMEM as i64) == 0 {
             state.push_str("W");
         }
-        if info.nice < bsd_kvm_sys::NZERO as i8 || info.pri.class == bsd_kvm_sys::PRI_REALTIME as u8
-        {
+        if info.nice < libc::NZERO as i8 || info.pri.class == bsd_kvm_sys::PRI_REALTIME as u8 {
             state.push_str("<");
         }
-        if info.nice > bsd_kvm_sys::NZERO as i8 || info.pri.class == bsd_kvm_sys::PRI_IDLE as u8 {
+        if info.nice > libc::NZERO as i8 || info.pri.class == bsd_kvm_sys::PRI_IDLE as u8 {
             state.push_str("N");
         }
-        if (flag & bsd_kvm_sys::P_TRACED as i64) != 0 {
+        if (flag & libc::P_TRACED as i64) != 0 {
             state.push_str("X");
         }
-        if (flag & bsd_kvm_sys::P_WEXIT as i64) != 0 && info.stat != bsd_kvm_sys::SZOMB as i8 {
+        if (flag & libc::P_WEXIT as i64) != 0 && info.stat != libc::SZOMB as i8 {
             state.push_str("E");
         }
-        if (flag & bsd_kvm_sys::P_PPWAIT as i64) != 0 {
+        if (flag & libc::P_PPWAIT as i64) != 0 {
             state.push_str("V");
         }
-        if (flag & bsd_kvm_sys::P_SYSTEM as i64) != 0 || info.lock > 0 {
+        if (flag & libc::P_SYSTEM as i64) != 0 || info.lock > 0 {
             state.push_str("L");
         }
         if (cr_flags & bsd_kvm_sys::CRED_FLAG_CAPMODE) != 0 {
             state.push_str("C");
         }
-        if (kiflag & bsd_kvm_sys::KI_SLEADER as i64) != 0 {
+        if (kiflag & libc::KI_SLEADER as i64) != 0 {
             state.push_str("s");
         }
-        if (flag & bsd_kvm_sys::P_CONTROLT as i64) != 0 && info.pgid == info.tpgid {
+        if (flag & libc::P_CONTROLT as i64) != 0 && info.pgid == info.tpgid {
             state.push_str("+");
         }
-        if (flag & bsd_kvm_sys::P_JAILED as i64) != 0 {
+        if (flag & libc::P_JAILED as i64) != 0 {
             state.push_str("J");
         }
         let fmt_content = state;
