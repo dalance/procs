fe41b4e60ac1ca3410da42ebfe64dbf7c942bc78
dalance <dalance@gmail.com>
2025-01-06 17:06:51 +0900

    Fix FreeBSD build failure

--- src/columns/state.rs
+++ src/columns/state.rs
@@ -129,7 +129,7 @@ impl Column for State {
         if (flag & libc::P_SYSTEM as i64) != 0 || info.lock > 0 {
             state.push_str("L");
         }
-        if (cr_flags & bsd_kvm_sys::CRED_FLAG_CAPMODE) != 0 {
+        if (cr_flags & libc::KI_CRF_CAPABILITY_MODE as u32) != 0 {
             state.push_str("C");
         }
         if (kiflag & libc::KI_SLEADER as i64) != 0 {
